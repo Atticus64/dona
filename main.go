324ed22dda8816d82e3f2dc3d864b39a556130c5
Jonathan <jonathanelian64@gmail.com>
2023-08-06 22:25:30 -0700

    feat: add -v version flag

--- main.go
+++ main.go
@@ -13,11 +13,25 @@ var rootCmd = &cobra.Command{
 	Short: "Dona 🍩 is a very fast dotfile manager",
 	Long:  `CLI to manage your dotfiles`,
 	Run: func(cmd *cobra.Command, args []string) {
+		ShowVersion, error := cmd.Flags().GetBool("version")
+		if error != nil {
+			fmt.Println(error)
+		}
+
+		if ShowVersion {
+			fmt.Println("Dona version 0.0.1 beta ultra alpha radioactive")
+			return
+		}
+
 		fmt.Println("Dona cli to manage your dotfiles")
 	},
 }
 
+var ShowVersion bool
+
 func main() {
+	rootCmd.PersistentFlags().BoolVarP(&ShowVersion, "version", "v", false, "Show Dona version")
+
 	rootCmd.AddCommand(cmd.VersionCmd)
 	rootCmd.AddCommand(cmd.SearchCmd)
 
