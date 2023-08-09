6ff885fc65af73fdf2451e420fcab5cf56be525a
Jonathan <jonathanelian64@gmail.com>
2023-08-08 20:21:31 -0600

    fix: better aproach in install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ green="\e[0;32m"
 install_dir="$HOME/.dona/bin"
 
 case $(uname -sm) in
-	"Darwin x86_64") target="x86_64_news_macos"
+	"Darwin x86_64") target="x86_64_dona_macos"
 		install_dir="/usr/local/bin" ;;
-	"Darwin arm64") target="aarch64_news_macos" ;;
+	"Darwin arm64") target="aarch64_dona_macos" ;;
 	*) target="x86_64_linux_dona" ;;
 esac
 
@@ -20,17 +20,18 @@ fi
 
 printf "\n\nFetching binary dona\n"
 
-curl --fail --location --progress-bar --output dona https://github.com/Atticus64/dona/releases/download/v$version/$target
+
+curl --fail --progress-bar --location --output dona https://github.com/Atticus64/dona/releases/download/v$version/$target
 
 chmod +x $bin
 
 mv $bin $install_dir
 
 if command -v news >/dev/null; then
-        printf "\e[0;33mNews\e[0m updated \e[0;32msuccesfully\e[0m\n"
+        printf "\e[0;33mDona\e[0m updated \e[0;32msuccesfully\e[0m\n"
         printf "Run 'dona --help' to get started\n"
 else
-  printf "\e[0;33mNews\e[0m installed \e[0;32msuccesfully\e[0m\n"
+  printf "\e[0;33mDona\e[0m installed \e[0;32msuccesfully\e[0m\n"
   case $SHELL in
         /bin/zsh)
                 shell_profile="~/.zshrc"
