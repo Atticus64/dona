37ee0405ce013169dc99f3ad7fb8a6e28cea1dfb
Jonathan <jonathanelian64@gmail.com>
2026-03-25 07:22:13 -0700

    feat: agregar test de prueba

---
+++ tests/name_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func Saludar(nombre string) string {
+	saludo := fmt.Sprintf("Hola %s", nombre)
+	return saludo
+}
+
+func TestHelloName(t *testing.T) {
+    name := "Juan Carlos Bodoque"
+    want := regexp.MustCompile(`\b`+name+`\b`)
+	// Must be wrong 
+    // msg := Saludar("Pedro Paramo")
+	// Must be good 
+    msg := Saludar("Juan Carlos Bodoque")
+    if !want.MatchString(msg) {
+        t.Errorf(`Saludar("Juan Carlos Bodoque") = %q, want match for %#q, nil`, msg, want)
+    }
+}
